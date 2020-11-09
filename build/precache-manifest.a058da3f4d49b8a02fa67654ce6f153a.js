de093eea202bf52523c7448312baf9215ed3c3ba
Eduardo Mendoza <mendozae96@gmail.com>
2020-11-09 15:15:54 -0600

    ran build

---
+++ build/precache-manifest.a058da3f4d49b8a02fa67654ce6f153a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2980d8488893ad5655917c22ad5fa531",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1980362292e6d08f7374",
+    "url": "/static/css/main.66c9fe56.chunk.css"
+  },
+  {
+    "revision": "3dc662d248bf4bdd7a9e",
+    "url": "/static/js/2.2d4c3583.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.2d4c3583.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1980362292e6d08f7374",
+    "url": "/static/js/main.fa561a18.chunk.js"
+  },
+  {
+    "revision": "3e150b3981cf25677ace",
+    "url": "/static/js/runtime-main.d29f321d.js"
+  },
+  {
+    "revision": "cccfd52c3356933f35eadcfdc625a43d",
+    "url": "/static/media/Doge.cccfd52c.jpg"
+  }
+]);
